279f3a41e30392b53a05455aa8b72a037650fd47
SvenRobbie <hobby@c14h19no2.nl>
2025-11-18 09:19:06 +0100

    Pas Key_gen aan, zodat ie ook ECC kan genereren.

--- Sven's mappie/Github_API_keys.py
+++ Sven's mappie/Github_API_keys.py
@@ -1,7 +1,16 @@
-"""
-Generate API keys for pulling the Priv key from github. Load to mem. but make sure to NOT LOAD in unecrypted way.
-Make sure to ONLY load IF payment is made.
 
+
+
+
+
+
+
+"""
+- Generate API keys for pulling the Priv key from github. Load to mem. but make sure to NOT LOAD in unecrypted way.
+- Make sure to ONLY load IF payment is made.
+- Priv key for access to code in the repo, download Priv decrypt key with that.
+    - How to keep secure? - Download from git?
+ 
 
 
 
