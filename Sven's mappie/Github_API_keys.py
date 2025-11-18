cf69c307fcc2584b72a7d4bb27e95bc9ac16c789
SvenRobbie <hobby@c14h19no2.nl>
2025-11-18 08:58:13 +0100

    Effe een test mapje

---
+++ Sven's mappie/Github_API_keys.py
@@ -0,0 +1,11 @@
+"""
+Generate API keys for pulling the Priv key from github. Load to mem. but make sure to NOT LOAD in unecrypted way.
+Make sure to ONLY load IF payment is made.
+
+
+
+
+
+
+
+"""
